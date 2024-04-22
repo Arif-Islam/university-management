e2148ff25124676e1e78251b60caefc74f3c9434
Arif <iariful950@gmail.com>
2024-04-23 01:32:17 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Working Successfully')
+// Application routes
+app.use('/api/v1/users/', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
 })
 
-export default app;
+export default app
